5d4e60b413c01b498c3626a169f615b25315ed9d
rosh <rosh@balink.net>
2014-05-14 13:14:32 +0300

    update

--- FileUploadMVC4/Controllers/HomeController.cs
+++ FileUploadMVC4/Controllers/HomeController.cs
@@ -5,57 +5,111 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FileUploadMVC4.Models;
+using System.Net;
+using System.Xml.Linq;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace FileUploadMVC4.Controllers
 {
-	public class HomeController : Controller
-	{
-		//
-		// GET: /Home/
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
 
-		public ActionResult Index()
-		{
-			return View();
-		}
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public FilePathResult Image()
-		{
-			string filename = Request.Url.AbsolutePath.Replace("/home/image", "");
-			string contentType = "";
-			var filePath = new FileInfo(Server.MapPath("~/App_Data") + filename);
+        public FilePathResult Image()
+        {
+            string filename = Request.Url.AbsolutePath.Replace("/home/image", "");
+            string contentType = "";
+            var filePath = new FileInfo(Server.MapPath("~/App_Data") + filename);
 
-			var index = filename.LastIndexOf(".") + 1;
-			var extension = filename.Substring(index).ToUpperInvariant();
+            var index = filename.LastIndexOf(".") + 1;
+            var extension = filename.Substring(index).ToUpperInvariant();
 
-			// Fix for IE not handling jpg image types
-			contentType = string.Compare(extension, "JPG") == 0 ? "image/jpeg" : string.Format("image/{0}", extension);
+            // Fix for IE not handling jpg image types
+            contentType = string.Compare(extension, "JPG") == 0 ? "image/jpeg" : string.Format("image/{0}", extension);
 
-			return File(filePath.FullName, contentType);
-		}
+            return File(filePath.FullName, contentType);
+        }
 
-		[HttpPost]
-		public ContentResult UploadFiles()
-		{
-			var r = new List<UploadFilesResult>();
+        [HttpPost]
+        public ContentResult UploadFiles()
+        {
+            //https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin
+            string imgurl = string.Empty;
+            HttpPostedFileBase img = Request.Files[0] as HttpPostedFileBase;
+            if (img != null)
+            {
+                string pic = System.IO.Path.GetFileName(img.FileName);
+                //url = UploadImage(file.FileName);
 
-			foreach (string file in Request.Files)
-			{
-				HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
-				if (hpf.ContentLength == 0)
-					continue;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    img.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                    imgurl = UploadImageToImgur(array);
 
-				string savedFileName = Path.Combine(Server.MapPath("~/App_Data"), Path.GetFileName(hpf.FileName));
-				hpf.SaveAs(savedFileName);
+                }
 
-				r.Add(new UploadFilesResult()
-				{
-					Name = hpf.FileName,
-					Length = hpf.ContentLength,
-					Type = hpf.ContentType
-				});
-			}
-			return Content("{\"name\":\"" + r[0].Name + "\",\"type\":\"" + r[0].Type + "\",\"size\":\"" + string.Format("{0} bytes", r[0].Length) + "\"}", "application/json");
-		}
+            }
+            var res = new UploadFilesResult()
+            {
+                name = img.FileName,
+                size = img.ContentLength,
+                type = img.ContentType,
+                url = imgurl
+            };
 
-	}
+            //var r = new List<UploadFilesResult>();
+            //foreach (string file in Request.Files)
+            //{
+            //    HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+            //    if (hpf.ContentLength == 0)
+            //        continue;
+
+            //    string savedFileName = Path.Combine(Server.MapPath("~/App_Data"), Path.GetFileName(hpf.FileName));
+            //    hpf.SaveAs(savedFileName);
+
+            //    r.Add(new UploadFilesResult()
+            //    {
+            //        name = hpf.FileName,
+            //        size = hpf.ContentLength,
+            //        type = hpf.ContentType
+            //    });
+            //}
+            string json = JsonConvert.SerializeObject(res);
+            //string s = "{\"name\":\"" + r[0].name + "\",\"type\":\"" + r[0].type + "\",\"size\":\"" + string.Format("{0} bytes", r[0].size) + "\"}";
+            return Content(json, "application/json");
+
+            //return Content("{\"name\":\"" + r[0].Name + "\",\"type\":\"" + r[0].Type + "\",\"size\":\"" + string.Format("{0} bytes", r[0].Length) + "\"}", "application/json");
+        }
+
+        string ClientId = "6b18f55eeee07f1";
+        public string UploadImageToImgur(byte[] image)
+        {
+            WebClient w = new WebClient();
+            w.Headers.Add("Authorization", "Client-ID " + ClientId);
+            System.Collections.Specialized.NameValueCollection Keys = new System.Collections.Specialized.NameValueCollection();
+            try
+            {
+                Keys.Add("image", Convert.ToBase64String(image));
+                byte[] responseArray = w.UploadValues("https://api.imgur.com/3/image.xml", Keys);
+                dynamic result = Encoding.ASCII.GetString(responseArray);
+                XDocument xml = XDocument.Parse(result);
+                var i = xml.Root.Element("link").Value;
+                return i;
+            }
+            catch (Exception s)
+            {
+                //MessageBox.Show("Something went wrong. " + s.Message);
+                return s.Message;
+            }
+        }
+
+    }
 }
--- FileUploadMVC4/Models/UploadFilesResult.cs
+++ FileUploadMVC4/Models/UploadFilesResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,10 @@ namespace FileUploadMVC4.Models
 {
 	public class UploadFilesResult
 	{
-		public string Name { get; set; }
-		public int Length { get; set; }
-		public string Type { get; set; }
+		public string name { get; set; }
+        public int size { get; set; }
+		public string type { get; set; }
+
+        public string url { get; set; }
 	}
 }
